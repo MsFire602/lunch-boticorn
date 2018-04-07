d7169956898c13877fb0398b46866bdd5bb105a9
MsFire602 <31006774+MsFire602@users.noreply.github.com>
2018-04-07 16:02:28 +0300

    Allergens to index.js
    Hard-coded values, first version

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import request from 'request';
 
 import logger from './log';
 
-const menuUrl = 'http://pompier.fi/albertinkatu/lounas/';
-const weekdays = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai'];
+const menuUrl = 'http://www.ravintolafactory.com/aleksi/lounas/';
+const weekdays = ['Maanantai', 'Tiistai', 'Keskiviikko', 'Torstai', 'Perjantai', 'Lauantai'];
+const allergens = ['(L+G)', '(M+G)', '(L)', '(L+VS)', '(M+G+VS+VE)'];
+const userAllergens = 'L+G';
 
-const parsePompierMenu = (html) => {
+const parseAleksiMenu = (html) => {
   const dayIndex = new Date().getDay() - 1;
   if (dayIndex < 0 || dayIndex >= weekdays.length) {
     return 'Lunch available only weekdays!';
@@ -14,33 +16,54 @@ const parsePompierMenu = (html) => {
 
   // Parse menu text
   const jsdom = new JSDOM(html);
-  const cssSelector = '.page-content';
+  const cssSelector = '.content-page';
   const menu = jsdom.window.document.querySelector(cssSelector).textContent;
 
   // Find correct substring from menu
   const startIndex = menu.indexOf(weekdays[dayIndex]);
   const endIndex = dayIndex < weekdays.length - 1
     ? menu.indexOf(weekdays[dayIndex + 1])
-    : menu.length;
+    : menu.indexOf('Lauantailounas');
   if (startIndex >= 0 && startIndex < endIndex) {
     const linesString = menu.substring(startIndex, endIndex);
-    const lines = linesString.split('\n');
+    const lineString = parseMenuAllergens(linesString);
+    const lines = lineString.split('\n');
 
     // Format output
     return [`<${menuUrl}|${lines[0]}>`, ...lines.slice(1, lines.length)].join('\n');
   }
 
-  return 'Error fetching Pompier lunch information';
+  return 'Error fetching Aleksi lunch information';
 };
 
-const fetchPompierMenu = async (url = menuUrl) => new Promise((resolve) => {
+const parseMenuAllergens = (text) => {
+  var linesText = text.split(')');
+  var outputText = '';
+
+  for(var ii = 0, len = linesText.length; ii < len; ii++) {
+    var currentLine = linesText[ii];
+    if(ii != len - 1){
+      currentLine += ')';
+    }
+
+    if(currentLine.includes(userAllergens))
+    {
+      outputText += currentLine + '\n';
+    }
+  }
+
+  logger.info('-Allergens:' + outputText);
+  return outputText;
+};
+
+const fetchAleksiMenu = async (url = menuUrl) => new Promise((resolve) => {
   request(
     url,
     (error, { statusCode }, html) => {
       if (!error && statusCode === 200) {
-        return resolve(parsePompierMenu(html));
+        return resolve(parseAleksiMenu(html));
       }
-      const errorMsg = `Failed fetching Pompier menu: ${statusCode}`;
+      const errorMsg = `Failed fetching Aleksi menu: ${statusCode}`;
       logger.error(errorMsg);
       return resolve(errorMsg);
     },
@@ -48,13 +71,13 @@ const fetchPompierMenu = async (url = menuUrl) => new Promise((resolve) => {
 });
 
 (async () => {
-  logger.info('Fetching Pompier menu for today...');
-  const text = await fetchPompierMenu();
-  logger.info(`Result:\n${text}`);
-  const payload = { text };
-  request({
-    uri: process.env.SLACK_WEBHOOK,
-    method: 'POST',
-    json: payload,
-  }, () => logger.info('Posted menu to slack!'));
-})();
+    logger.info('Fetching Aleksi menu for today...');
+    const text = await fetchAleksiMenu();
+    logger.info(`Result:\n${text}`);
+    const payload = { text };
+    request({
+      uri: 'https://hooks.slack.com/services/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',
+      method: 'POST',
+      json: payload,
+    }, () => logger.info('Posted menu to slack!'));
+  })();
